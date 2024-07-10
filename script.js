d934a86dcaab85ecb918c293d56e1ee067834805
Rad-spencer <rajkumar.rksen@gmail.com>
2024-07-10 16:56:25 +0545

    making of page 3 remaining

--- script.js
+++ script.js
@@ -1,4 +1,30 @@
 
+
+function locomotiveanimation(){
+    gsap.registerPlugin(ScrollTrigger);
+
+// Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+
+const locoScroll = new LocomotiveScroll({
+  el: document.querySelector("#main"),
+  smooth: true
+});
+// each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+locoScroll.on("scroll", ScrollTrigger.update);
+
+// tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
+ScrollTrigger.scrollerProxy("#main", {
+  scrollTop(value) {
+    return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+  }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+  getBoundingClientRect() {
+    return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
+  },
+  // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+  pinType: document.querySelector("#main").style.transform ? "transform" : "fixed"
+});
+}
+
 function loadingloaderanimation(){
     let NowAnimation = document.querySelector(".line h2");
 let timer = document.querySelector("#line1-part1 h5");
@@ -35,7 +61,7 @@ opacity:1,
 tl.to("#loader",{
 opacity:0,
 duration:0.2,
-delay:0
+delay:4,
 })
 tl.from("#page1",{
 delay:0.2,
@@ -59,21 +85,21 @@ tl.from("#hero1 h1, #hero2 h2, #hero3 h1, #hero4 h1",{
 
 }
 
-function cursoranimation(){
-    document.addEventListener("mousemove",(dets)=>{
-        gsap.to("#crsr",{
-            left:dets.x,
-            top:dets.y,
-        })
-    })
+// function cursoranimation(){
+//     document.addEventListener("mousemove",(dets)=>{
+//         gsap.to("#crsr",{
+//             left:dets.x,
+//             top:dets.y,
+//         })
+//     })
     
-    Shery.makeMagnet("#nav-part2 h4" /* Element to target.*/, {
+//     Shery.makeMagnet("#nav-part2 h4" /* Element to target.*/, {
         
-      });
-}
+//       });
+// }
 
 
 
-
+locomotiveanimation();
 loadingloaderanimation();
 // cursoranimation();
